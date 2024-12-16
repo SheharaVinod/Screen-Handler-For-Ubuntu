132fd0f98b15b17fd6a2a2cd2cb8cddfbb68d1ad
shehara winod <shehara.winod123@gmail.com>
2024-12-16 06:55:54 +0530

    More bug fix

--- Subprocess/logger.py
+++ Subprocess/logger.py
@@ -12,7 +12,6 @@ class Logger:
     def create_log():
         try:
             os.chdir(os.path.abspath(os.path.expanduser(Logger._data_path)))
-
             os.makedirs("logs", exist_ok=True)
             with open("logs/" + Logger._current_log, 'w') as f:
                 f.write('# ----------------------- Logs ----------------------- \n')
@@ -23,7 +22,7 @@ class Logger:
     @staticmethod
     def log(massage):
         # "[ScreenManager] at mm/dd  HH:MM:SS -> "
-        os.chdir(Logger._data_path)
+        os.chdir(os.path.abspath(os.path.expanduser(Logger._data_path)))
 
         time = datetime.datetime.now()
         log_prefix = f"{Logger._prefix} at {time.month}/{time.day} {time.hour}:{time.minute}:{time.second} -> "
